cf5df608f41909ac455dad86ad831b0c540e8427
Sebastian Gumprich <sebastian.gumprich@t-systems.com>
2020-07-10 08:34:13 +0200

    update bash generate script for new testing paths

--- hacking/update_examples_and_tests.sh
+++ hacking/update_examples_and_tests.sh
@@ -3,7 +3,7 @@
 for module in ../plugins/modules/*.py; do
     module_name="$(basename "${module}" .py)"
 
-    echo "---" | tee "../tests/integration/${module_name}.yml" "../examples/${module_name}.yml"
+    echo "---" | tee "../tests/integration/targets/icinga/tasks/${module_name}.yml" "../examples/${module_name}.yml"
     # https://stackoverflow.com/a/22221307
-    sed -n '/EXAMPLES/,/"""/{/EXAMPLES/b;/"""/b;p}' "${module}" | tee -a "../examples/${module_name}.yml" "../tests/integration/${module_name}.yml"
+    sed -n '/EXAMPLES/,/"""/{/EXAMPLES/b;/"""/b;p}' "${module}" | tee -a "../examples/${module_name}.yml" "../tests/integration/targets/icinga/tasks/${module_name}.yml"
 done
