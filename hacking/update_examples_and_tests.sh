98e3238e50a32b734874d1ab5d91ae419ef6c9f1
Sebastian Gumprich <sebastian.gumprich@t-systems.com>
2020-10-21 16:05:10 +0200

    fix linting for tests

--- hacking/update_examples_and_tests.sh
+++ hacking/update_examples_and_tests.sh
@@ -4,63 +4,43 @@ for module in ../plugins/modules/*.py; do
     module_name="$(basename "${module}" .py)"
 
     # create examples
-    echo "---" | tee "../examples/${module_name}.yml" 1> /dev/null
     # https://stackoverflow.com/a/22221307
-    sed -n '/EXAMPLES/,/"""/{/EXAMPLES/b;/"""/b;p}' "${module}" | tee -a "../examples/${module_name}.yml" 1> /dev/null
-    yq r -d* "../examples/${module_name}.yml" | sponge "../examples/${module_name}.yml"
+    sed -n '/EXAMPLES/,/"""/{/EXAMPLES/b;/"""/b;p}' "${module}" | tee "/tmp/${module_name}.yml" 1> /dev/null
+    length=$(($(yq r --length "/tmp/${module_name}.yml") - 1))
 
-    # create tests
-    echo "---" | tee "../tests/integration/targets/icinga/roles/icinga/tasks/${module_name}.yml" 1> /dev/null
-    sed -n '/EXAMPLES/,/"""/{/EXAMPLES/b;/"""/b;p}' "${module}" | tee -a "../tests/integration/targets/icinga/roles/icinga/tasks/${module_name}.yml" 1> /dev/null
+    echo  "---" | tee "../examples/${module_name}.yml" "../tests/integration/targets/icinga/roles/icinga/tasks/${module_name}.yml" "../tests/integration/targets/icinga/roles/icinga/tasks/absent_${module_name}.yml" "../tests/integration/targets/icinga/roles/icinga/tasks/wrong_pass_${module_name}.yml" "../tests/integration/targets/icinga/roles/icinga/tasks/wrong_host_${module_name}.yml" 1> /dev/null
 
-    yq r -d* "../tests/integration/targets/icinga/roles/icinga/tasks/${module_name}.yml" | sponge "../tests/integration/targets/icinga/roles/icinga/tasks/${module_name}.yml"
+    for item in $(seq 0 ${length}); do
+      yq r --printMode v --collect "/tmp/${module_name}.yml" "[$item]" | tee -a "../examples/${module_name}.yml" "../tests/integration/targets/icinga/roles/icinga/tasks/${module_name}.yml" "../tests/integration/targets/icinga/roles/icinga/tasks/absent_${module_name}.yml" "../tests/integration/targets/icinga/roles/icinga/tasks/wrong_pass_${module_name}.yml" "../tests/integration/targets/icinga/roles/icinga/tasks/wrong_host_${module_name}.yml" 1> /dev/null
 
-    # create working tests deleting the hosts
-    echo "---" | tee "../tests/integration/targets/icinga/roles/icinga/tasks/absent_${module_name}.yml" 1> /dev/null
-    sed -n '/EXAMPLES/,/"""/{/EXAMPLES/b;/"""/b;p}' "${module}" | tee -a "../tests/integration/targets/icinga/roles/icinga/tasks/absent_${module_name}.yml" 1> /dev/null
-    yq w -d* -i "../tests/integration/targets/icinga/roles/icinga/tasks/absent_${module_name}.yml" "[].${module_name}.state" "absent"
+      # add an assert task that checks for failure
+      yq m -a -i "../tests/integration/targets/icinga/roles/icinga/tasks/wrong_pass_${module_name}.yml" merge.yml
+      yq m -a -i "../tests/integration/targets/icinga/roles/icinga/tasks/wrong_host_${module_name}.yml" merge.yml
 
-    # delete imports and command from the tests, because they aren't necessary to delete an object
-    # regression test for https://github.com/T-Systems-MMS/ansible-collection-icinga-director/issues/44
-    yq d -d* -i "../tests/integration/targets/icinga/roles/icinga/tasks/absent_${module_name}.yml" ".${module_name}.imports"
-    yq d -d* -i "../tests/integration/targets/icinga/roles/icinga/tasks/absent_${module_name}.yml" ".${module_name}.command"
+      # create working tests deleting the hosts
+      yq w -i "../tests/integration/targets/icinga/roles/icinga/tasks/absent_${module_name}.yml" "[].${module_name}.state" "absent"
 
-    yq r -d* "../tests/integration/targets/icinga/roles/icinga/tasks/absent_${module_name}.yml" | sponge "../tests/integration/targets/icinga/roles/icinga/tasks/absent_${module_name}.yml"
+      # delete imports and command from the tests, because they aren't necessary to delete an object
+      # regression test for https://github.com/T-Systems-MMS/ansible-collection-icinga-director/issues/44
+      yq d -i "../tests/integration/targets/icinga/roles/icinga/tasks/absent_${module_name}.yml" "(name==*).${module_name}.imports"
+      yq d -i "../tests/integration/targets/icinga/roles/icinga/tasks/absent_${module_name}.yml" "(name==*).${module_name}.command"
 
-    # create failing tests with wrong password
-    echo "---" | tee "../tests/integration/targets/icinga/roles/icinga/tasks/wrong_pass_${module_name}.yml" 1> /dev/null
-    sed -n '/EXAMPLES/,/"""/{/EXAMPLES/b;/"""/b;p}' "${module}" | tee -a "../tests/integration/targets/icinga/roles/icinga/tasks/wrong_pass_${module_name}.yml" 1> /dev/null
+      # create failing tests with wrong password
+      # replace password variable with wrong password
+      yq w -i "../tests/integration/targets/icinga/roles/icinga/tasks/wrong_pass_${module_name}.yml" "(name==*).${module_name}.url_password" "iamwrong"
 
-    # replace password variable with wrong password
-    yq w -d* -i "../tests/integration/targets/icinga/roles/icinga/tasks/wrong_pass_${module_name}.yml" ".${module_name}.url_password" "iamwrong"
+      # add ignore_errors to the creation-task
+      yq w -i "../tests/integration/targets/icinga/roles/icinga/tasks/wrong_pass_${module_name}.yml" "(name==*).ignore_errors" "true"
+      yq w -i "../tests/integration/targets/icinga/roles/icinga/tasks/wrong_pass_${module_name}.yml" "(name==*).register" "result"
 
-    # add ignore_errors to the creation-task
-    yq w -d* -i "../tests/integration/targets/icinga/roles/icinga/tasks/wrong_pass_${module_name}.yml" "[].ignore_errors" "true"
-    yq w -d* -i "../tests/integration/targets/icinga/roles/icinga/tasks/wrong_pass_${module_name}.yml" "[].register" "result"
+      # create failing tests with wrong host
+      # replace url varuable with nonexisting url
+      yq w -i "../tests/integration/targets/icinga/roles/icinga/tasks/wrong_host_${module_name}.yml" "(name==*).${module_name}.url" "http://nonexistant"
 
-    # add an assert task that checks for failure
-    yq m -d* -a -i "../tests/integration/targets/icinga/roles/icinga/tasks/wrong_pass_${module_name}.yml" merge.yml
+      # add an assert task that checks for failure
+      yq w -i "../tests/integration/targets/icinga/roles/icinga/tasks/wrong_host_${module_name}.yml" "(name==*).ignore_errors" "true"
 
-    #
-    yq r -d* "../tests/integration/targets/icinga/roles/icinga/tasks/wrong_pass_${module_name}.yml" | sponge "../tests/integration/targets/icinga/roles/icinga/tasks/wrong_pass_${module_name}.yml"
-
-    # create failing tests with wrong host
-    # add test
-    echo "---" | tee "../tests/integration/targets/icinga/roles/icinga/tasks/wrong_host_${module_name}.yml" 1> /dev/null
-    sed -n '/EXAMPLES/,/"""/{/EXAMPLES/b;/"""/b;p}' "${module}" | tee -a "../tests/integration/targets/icinga/roles/icinga/tasks/wrong_host_${module_name}.yml" 1> /dev/null
-
-    # replace url varuable with nonexisting url
-    yq w -d* -i "../tests/integration/targets/icinga/roles/icinga/tasks/wrong_host_${module_name}.yml" ".${module_name}.url" "http://nonexistant"
-
-    # add an assert task that checks for failure
-    yq w -d* -i "../tests/integration/targets/icinga/roles/icinga/tasks/wrong_host_${module_name}.yml" "[].ignore_errors" "true"
-
-    # add ignore_errors to the creation-task
-    yq w -d* -i "../tests/integration/targets/icinga/roles/icinga/tasks/wrong_host_${module_name}.yml" "[].register" "result"
-
-    # add an assert task that checks for failure
-    yq m -d* -a -i "../tests/integration/targets/icinga/roles/icinga/tasks/wrong_host_${module_name}.yml" merge.yml
-
-    yq r -d* "../tests/integration/targets/icinga/roles/icinga/tasks/wrong_host_${module_name}.yml" | sponge "../tests/integration/targets/icinga/roles/icinga/tasks/wrong_host_${module_name}.yml"
-
+      # add ignore_errors to the creation-task
+      yq w -i "../tests/integration/targets/icinga/roles/icinga/tasks/wrong_host_${module_name}.yml" "(name==*).register" "result"
+    done
 done
