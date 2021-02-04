48adbf19e2187e23fbcb95be6deca219b5f6caf4
Sebastian Gumprich <sebastian.gumprich@t-systems.com>
2021-02-04 10:46:05 +0100

    add more test executions to trigger all paths

--- tests/integration/targets/icinga/runme.sh
+++ tests/integration/targets/icinga/runme.sh
@@ -5,3 +5,5 @@ set -eux
 ansible-playbook normalmode.yml
 ansible-playbook checkmode.yml
 ansible-playbook checkmode.yml --check --diff
+ansible-playbook checkmode_2.yml
+ansible-playbook checkmode_2.yml --check --diff
