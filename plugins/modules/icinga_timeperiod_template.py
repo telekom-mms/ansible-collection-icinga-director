4791c71dc5bea8fecefb74ea0feab555d0afe3cd
Sebastian Gumprich <rndmh3ro@users.noreply.github.com>
2021-05-09 21:48:27 +0200

    Update plugins/modules/icinga_timeperiod_template.py
    Co-authored-by: schurzi <Martin.Schurz@t-systems.com>

--- plugins/modules/icinga_timeperiod_template.py
+++ plugins/modules/icinga_timeperiod_template.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 #
-# Copyright (c) 2020 T-Systems Multimedia Solutions GmbH
+# Copyright (c) 2021 T-Systems Multimedia Solutions GmbH
 # GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
 #
 # This module is free software: you can redistribute it and/or modify
