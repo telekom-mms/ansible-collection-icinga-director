da066d583476a138080cb91d1955a8bd0e8e4ac9
Sebastian Gumprich <sebastian.gumprich@t-systems.com>
2021-11-25 08:42:24 +0100

    verbose and diff output in tests

--- tests/integration/targets/icinga/runme.sh
+++ tests/integration/targets/icinga/runme.sh
@@ -2,8 +2,8 @@
 
 set -eux
 
-ansible-playbook normalmode.yml
-ansible-playbook checkmode.yml
-ansible-playbook checkmode.yml --check --diff
-ansible-playbook checkmode_2.yml
-ansible-playbook checkmode_2.yml --check --diff
+ansible-playbook normalmode.yml -v --diff
+ansible-playbook checkmode.yml -v --diff
+ansible-playbook checkmode.yml --check --diff -v
+ansible-playbook checkmode_2.yml -v --diff
+ansible-playbook checkmode_2.yml --check --diff -v
